f69fcf8217a8a09aa03f07a6f5013d464f1e09b0
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-03-07 10:35:32 +0000

    PrintAlpha

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * main - Entry Point
+ * Return Always (Success)
+ */
+
+int main(void)
+
+{
+	char a;
+	
+	for(a = 'a' ; a <= 'z' ; a++)
+	putchar (a);
+	putchar ('\n');
+	return (0);
+}		
