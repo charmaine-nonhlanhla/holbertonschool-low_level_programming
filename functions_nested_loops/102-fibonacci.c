79edeebc26584a3c1cb6310e77a3561370924dea
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-03-08 13:32:24 +0000

    Fibonacci

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+
 /**
-* main - Print first 52 fibonacci numbers
-*
-* Return: nothing
-*/
+ * main - Print first 52 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-  int counter = 2;
-  long int a = 1;
-  long int b = a + 1;
-  long int c = a + b;
+	int counter = 2;
+	long int a = 1;
+	long int b = a + 1;
+	long int c = a + b;
 
-  printf("%ld, %ld, ", a, b);
-  while (counter < 50)
-  {
-    printf("%ld", c);
-    counter++;
-    a = b;
-    b = c;
-    c = a + b;
-    if (counter < 50)
-    {
-      printf(", ");
-    }
-  }
+	printf("%ld, %ld, ", a, b);
+	while (counter < 50)
+	{
+		printf("%ld", c);
+		counter++;
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 50)
+		{
+			printf(", ");
+		}
+	}
 
-  printf("\n");
-  return (0);
+	printf("\n");
+	return (0);
 }
