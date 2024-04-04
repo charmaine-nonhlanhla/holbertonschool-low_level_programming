108c9ab7ca84af7fd663aafcb9306f058687f0c6
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-04-04 12:19:26 +0000

    pointers

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,8 +1,7 @@
 #include "function_pointers.h"
-#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "3-calc.h"
 /**
  * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
@@ -32,7 +31,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	if ((*op == '/' && num2 == 0) ||
-	    (*op == '%' && num2 == 0))
+		(*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
