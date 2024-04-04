88e81ad89c9c7dba9ee1f39901e9ffd2a333264c
charmaine-nonhlanhla <cnmogotlane@gmail.com>
2024-04-04 07:13:10 +0000

    malloc free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,30 +1,44 @@
-#include "main.h"
+/*
+ * File: 2-str_concat.c
+ * Auth: Charmaine Mogotlane
+ */
+
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- *_strdup - allocate a string.
- *@str: string.
- *Return: pointer to string if success.
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ *
+ * Return: If concatenation fails - NULL.
+ *         Otherwise - a pointer the newly-allocated space in memory
+ *                     containing the concatenated strings.
  */
-
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	int i;
-	char *str1;
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
 
-	if (str == NULL)
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * len);
+
+	if (concat_str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i]; i++)
-	;
-	i++;
-	str1 = malloc(sizeof(char) * i);
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
 
-	if (str1 == NULL)
-		return (NULL);
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
 
-	for (i = 0; str[i] != '\0'; i++)
-		str1[i] = str[i];
-	str1[i] = '\0';
-	return (str1);
+	return (concat_str);
 }
